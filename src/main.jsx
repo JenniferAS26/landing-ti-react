5786cb846f091df340165017de206aad885d9e4f
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-09-13 19:46:40 -0500

    feat: add global styles and variables, install react-icons library, and change favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './component/AppRoutes'
+import './styles/global.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
