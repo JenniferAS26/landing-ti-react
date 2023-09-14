b0a1ed9fabeee299c6f8eeb5d92ae45dd1859332
vegadelalyra <lyradeperseo@gmail.com>
2023-09-14 01:35:17 -0500

    styles[header]: abort mouse interactions to improve website immersion

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import AppRoutes from './component/AppRoutes'
 import './styles/global.sass'
 
+// prevent regular experience to amplify web immersion
+document.oncontextmenu = () => false
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppRoutes />
