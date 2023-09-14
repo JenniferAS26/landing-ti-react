3a019ee72e2233f59222f2b4ff5e752c817aa5c4
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-09-14 16:43:16 -0500

    feat: New API for Gallery

--- src/component/App/index.jsx
+++ src/component/App/index.jsx
@@ -1,21 +1,30 @@
 // import Header from '../Header/Header'
 // import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 import Gallery from '../Gallery'
 import GalleryCard from "../GalleryCard";
 
 function App() {
+    const [cardDetails, setCardDetails] = useState([])
+
+    useEffect(() => {
+        fetch('https://api-workshop-3-production.up.railway.app/gallery')
+        .then(response => response.json())
+        .then(data => {
+            setCardDetails(data)
+            console.log(data)
+        })
+    }, [])
+    // console.log(cardDetails)
     return (
         <section className='wrapper'>
             {/* <Header /> */}
             <Gallery>
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
-                <GalleryCard />
+                {
+                    cardDetails.map((cardDetail, index) => (
+                        <GalleryCard key={index} cardDetail={cardDetail}  />
+                    ))
+                }
             </Gallery>
             {/* <Outlet /> */}
         </section>
--- src/component/GalleryCard/index.jsx
+++ src/component/GalleryCard/index.jsx
@@ -1,16 +1,21 @@
-import bannerMobile from '../../assets/mobile/image-deep-earth.jpg'
-import bannerDesktop from '../../assets/desktop/image-deep-earth.jpg'
+// import bannerMobile from '../../assets/mobile/image-deep-earth.jpg'
+// import bannerDesktop from '../../assets/desktop/image-deep-earth.jpg'
+import PropTypes from 'prop-types'
 import './styles.sass'
 
-const GalleryCard = () => {
+const GalleryCard = ({cardDetail}) => {
+  GalleryCard.propTypes = {
+    cardDetail: PropTypes.node.isRequired,
+  }
+
   return (
     <section className='gallery-card'>
-      <h3 className='gallery-card__title'>Name Complete</h3>
-      <picture>
-        <source media='(min-width: 1440px)' srcSet={bannerDesktop} />
+      <h3 className='gallery-card__title'>{cardDetail.name}</h3>
+      <picture className='gallery-card__image-container' >
+        <source media='(min-width: 1440px)' srcSet={cardDetail.desktop_image} />
         <img 
           className='gallery-card__image' 
-          src={bannerMobile} 
+          src={cardDetail.mobile_image} 
           alt='banner card image' 
         />
       </picture>
