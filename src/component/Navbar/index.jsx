bee4a3667c2ba8cad79ea1e175e0c8cde3768116
vegadelalyra <lyradeperseo@gmail.com>
2023-09-15 18:54:37 -0500

    feature: start navbar

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -5,13 +5,16 @@ import {
     NavbarContent,
     NavbarItem,
     Link,
-    Button,
 } from '@nextui-org/react'
 
 export default function NavBarTailWindNextUI() {
     return (
         <Navbar
-            className='bg-transparent absolute blur-0'
+            className='bg-transparent absolute text-white top-20 navbar-height'
+            style={{
+                backdropFilter: 'none !important',
+                WebkitBackdropFilter: 'none !important',
+            }}
             classNames={{
                 item: [
                     'flex',
@@ -20,22 +23,25 @@ export default function NavBarTailWindNextUI() {
                     'items-center',
                     "data-[active=true]:after:content-['']",
                     'data-[active=true]:after:absolute',
-                    'data-[active=true]:after:bottom-0',
+                    'data-[active=true]:after:bg-white',
+                    'data-[active=true]:after:top-7',
+                    'data-[active=true]:after:top-7',
                     'data-[active=true]:after:left-0',
                     'data-[active=true]:after:right-0',
-                    'data-[active=true]:after:h-[2px]',
+                    'data-[active=true]:after:h-[4px]',
                     'data-[active=true]:after:rounded-[2px]',
                     'data-[active=true]:after:bg-primary',
                 ],
             }}>
             <NavbarBrand>
-                <p className='font-bold text-black'>loopstudios</p>
+                <p className='font-bold'>loopstudios</p>
             </NavbarBrand>
             <NavbarContent
                 className='hidden sm:flex gap-4'
                 justify='center'>
                 <NavbarItem>
                     <Link
+                        className='text-white text-4xl'
                         color='foreground'
                         href='#'>
                         About
@@ -43,6 +49,7 @@ export default function NavBarTailWindNextUI() {
                 </NavbarItem>
                 <NavbarItem isActive>
                     <Link
+                        className='text-white text-4xl'
                         href='#'
                         aria-current='page'>
                         Careers
@@ -50,6 +57,7 @@ export default function NavBarTailWindNextUI() {
                 </NavbarItem>
                 <NavbarItem>
                     <Link
+                        className='text-white text-4xl'
                         color='foreground'
                         href='#'>
                         Events
@@ -57,6 +65,7 @@ export default function NavBarTailWindNextUI() {
                 </NavbarItem>
                 <NavbarItem>
                     <Link
+                        className='text-white text-4xl'
                         color='foreground'
                         href='#'>
                         Products
@@ -64,6 +73,7 @@ export default function NavBarTailWindNextUI() {
                 </NavbarItem>
                 <NavbarItem>
                     <Link
+                        className='text-white text-4xl'
                         color='foreground'
                         href='#'>
                         Support
