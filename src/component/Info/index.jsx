99261c9c44457d2cefbb98ec638dd564da0587fa
EstebanMa12 <desteban.maya@udea.edu.co>
2023-09-16 16:31:37 -0500

    an acceptable desktop design (y) B)

--- src/component/Info/index.jsx
+++ src/component/Info/index.jsx
@@ -19,34 +19,36 @@ const Info = () => {
 
     return (
         <section className='info-section'>
-            <figure>
-                <AdvancedImage
-                    width='100%'
-                    height='100%'
-
-                    cldImg={myImage}
-                    plugins={[
-                        lazyload({
-                            rootMargin: '10px 20px 10px 30px',
-                            threshold: 0.25,
-                        }),
-                        responsive({ 
-                            breakpoints: [200, 400, 600],
-                            maxWidth: 800,
-                        }),
-                        placeholder({ mode: 'vertalize' }),
-                    ]}
-                />
-            </figure>
-            <section className='info-section-write'> 
-                <h2 className='info-section-write__title'>THE LEADER IN INTERACTIVE VR</h2>
-                <article className='info-section-write__text'>
-                    Founded in 2011, Loopstudios has been producing world-class
-                    virtual reality projects for some of the best companies around
-                    the globe. Our award-winning creations have transformed
-                    businesses through digital experiences that bind to their brand.
-                </article>
-            </section>
+            <div className="info-section-container">
+                <figure>
+                    <AdvancedImage
+                        width='100%'
+                        height='100%'
+                        cldImg={myImage}
+                        plugins={[
+                            lazyload({
+                                rootMargin: '10px 20px 10px 30px',
+                                threshold: 0.25,
+                            }),
+                            responsive({
+                                breakpoints: [200, 400, 600],
+                                maxWidth: 800,
+                                steps: 200,
+                            }),
+                            placeholder({ mode: 'vertalize' }),
+                        ]}
+                    />
+                </figure>
+                <section className='info-section-write'>
+                    <h2 className='info-section-write__title'>THE LEADER IN INTERACTIVE VR</h2>
+                    <article className='info-section-write__text'>
+                        Founded in 2011, Loopstudios has been producing world-class
+                        virtual reality projects for some of the best companies around
+                        the globe. Our award-winning creations have transformed
+                        businesses through digital experiences that bind to their brand.
+                    </article>
+                </section>
+            </div>
         </section>
     )
 };
