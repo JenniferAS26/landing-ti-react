212219fc314bd75d542ffede4d17335751ef23fa
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-09-18 16:13:51 -0500

    Fix[Gallery]: Modify styles and add new breakpoint

--- src/component/App/index.jsx
+++ src/component/App/index.jsx
@@ -20,7 +20,7 @@ function App() {
             <Hero>
                 <NavBarTailWindNextUI />
             </Hero>
-            <Info />
+            {/* <Info /> */}
             <Gallery>
                 {cardDetails.map((cardDetail, index) => (
                     <GalleryCard
--- src/component/Gallery/index.jsx
+++ src/component/Gallery/index.jsx
@@ -8,11 +8,13 @@ const Gallery = ({ children }) => {
 
   return (
     <section className='gallery-section'>
-      <h2 className='gallery-section__title'>OUR CREATIONS</h2>
+      <div className='top-container'>
+        <h2 className='gallery-section__title'>OUR CREATIONS</h2>
+        <button className='gallery-section__button'>SEE ALL</button>
+      </div>
       <div className='gallery-section__cards-container'>
         {children}
       </div>
-      <button className='gallery-section__button'>SEE ALL</button>
     </section>
   )
 }
