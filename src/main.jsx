b97c4dd8c0e000beea8cb3963a4572e2b6b3c485
vegadelalyra <lyradeperseo@gmail.com>
2023-09-18 20:40:30 -0500

    hotfix: random comma remmoved (?

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <NextUIProvider>
     <BrowserRouter>
       <AppRoutes />
-    </BrowserRouter>,
+    </BrowserRouter>
   </NextUIProvider>
 )
